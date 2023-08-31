f9b16a8bb7870e2451ce67e7731c946003c65a70
unknown <arifmudzaqi777@gmail.com>
2023-08-31 10:41:02 +0700

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+    if (nums1.size() > nums2.size()) {
+        nums1.swap(nums2);
+    }
+
+    int m = nums1.size();
+    int n = nums2.size();
+    int low = 0, high = m;
+
+    while (low <= high) {
+        int partition1 = (low + high) / 2;
+        int partition2 = (m + n + 1) / 2 - partition1;
+
+        int maxLeft1 = (partition1 == 0) ? INT_MIN : nums1[partition1 - 1];
+        int minRight1 = (partition1 == m) ? INT_MAX : nums1[partition1];
+
+        int maxLeft2 = (partition2 == 0) ? INT_MIN : nums2[partition2 - 1];
+        int minRight2 = (partition2 == n) ? INT_MAX : nums2[partition2];
+
+        if (maxLeft1 <= minRight2 && maxLeft2 <= minRight1) {
+            if ((m + n) % 2 == 0) {
+                return (max(maxLeft1, maxLeft2) + min(minRight1, minRight2)) / 2.0;
+            } else {
+                return max(maxLeft1, maxLeft2);
+            }
+        } else if (maxLeft1 > minRight2) {
+            high = partition1 - 1;
+        } else {
+            low = partition1 + 1;
+        }
+    }
+    
+    return 0.0;  // This line should not be reached, just for safety.
+}
+
+int main() {
+    vector<int> nums1 = {1, 5, 9};
+    vector<int> nums2 = {2, 3, 6, 7};
+    cout << findMedianSortedArrays(nums1, nums2) << endl;  // Output: 5.0
+    
+    return 0;
+}
